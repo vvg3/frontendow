9a9fa9d46614d640fbc839d2537bf6fef55e51f2
Lawrence Wu <yl981@scarletmail.rutgers.edu>
2018-04-27 09:05:39 -0400

    for you vince

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+// import { Component } from '@angular/core';
+//
+// @Component({
+//   selector: 'pm-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent {
+//   title = 'Angular: Getting Started';
+// }
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+            <div><h1>{{pageTitle}}</h1>
+              <pm-userprofiles></pm-userprofiles>
+            </div>
+          `
 })
 export class AppComponent {
-  title = 'Angular: Getting Started';
+  pageTitle: string = 'OW Home Page';
 }
--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {UserprofileListComponent} from './userprofiles/userprofile-list.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserprofileListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
